3a61e29906042f705995119efc85957faf6708b8
Daniel Stefanov <babacheto@abv.bg>
2023-03-03 19:17:10 +0200

    last commit

--- air-bnb-example/src/components/App.js
+++ air-bnb-example/src/components/App.js
@@ -1,6 +1,7 @@
 import NavBar from './NavBar.js';
 import Hero from './Hero.js';
 import Card from './Card.js';
+import data from './data.js';
 
 function App() {
   return (
@@ -8,54 +9,11 @@ function App() {
       <NavBar />
       <Hero />
       <div className='ads'>
-        <Card
-          img="horseRiding.jpg"
-          rating="4.8"
-          reviewCount={14}
-          country="Usa"
-          title="Horse riding with Brooke Monk"
-          price={230} 
-          />
-          <Card
-          img="nickiMinaj.jpg"
-          rating="3.5"
-          reviewCount={14}
-          country="Usa"
-          title="Dancing Lessons with Nicki Minaj"
-          price={450} 
-          />
-          <Card
-          img="michaelPhelps.jpg"
-          rating="5.0"
-          reviewCount={6}
-          country="Usa"
-          title="Life Lessons with Michael Phelps"
-          price={336} 
-          />
-          <Card
-          img="krisko.jpg"
-          rating="5.0"
-          reviewCount={8}
-          country="Bulgaria"
-          title="Singing lessons with Krisko Biic"
-          price={410} 
-          />
-          <Card
-          img="cookingPhoto.jpg"
-          rating="4.0"
-          reviewCount={10}
-          country="Australia"
-          title="Cooking lessons with Margareth"
-          price={120} 
-          />
-          <Card
-          img="hacker.jpg"
-          rating="3.5"
-          reviewCount={6}
-          country="Russia"
-          title="Hacking lessons with Boris"
-          price={230} 
-          />
+        {data.map(x => <Card
+          key={x.id}
+          item={x}
+        />)}
+
       </div>
     </div>
   );
--- air-bnb-example/src/components/Card.js
+++ air-bnb-example/src/components/Card.js
@@ -1,21 +1,33 @@
 import React from "react"
 
 export default function Card(props) {
+
+    let badgeText = '';
+    if (props.openSpots === 0) {
+        badgeText = 'SOLD OUT';
+    }
+    else if (props.country === "Online") {
+        badgeText = "ONLINE"
+    }
+
+    let item= props.item;
+
     return (
         <div className="card">
 
-            <div class="image-container">
-                <img src={`../resources/${props.img}`} className="card--image" alt='creator' />
+            <div className="image-container">
+                {badgeText !== '' && <div className="card--badge">{badgeText}</div>}
+                <img src={`../resources/${item.coverImg}`} className="card--image" alt='creator' />
             </div>
             <div className="card--stats">
 
                 <img src='../resources/star.png' className="card--star" alt='star' />
-                <span>{props.rating}</span>
-                <span>({props.reviewCount}) • </span>
-                <span>{props.country}</span>
+                <span>{item.stats.rating}</span>
+                <span>({item.stats.reviewCount}) • </span>
+                <span>{item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{item.title}</p>
+            <p><span className="bold">From ${item.price}</span> / person</p>
         </div>
     )
 }
